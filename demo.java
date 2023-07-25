4b5a0ebb4b30bb5e6fc8bbd343e24331a6190d6c
shravann73 <shravann73@gmail.com>
2023-07-25 11:56:26 +0000

    java code

--- demo.java
+++ demo.java
@@ -6,3 +6,12 @@ class Demo
          s.o.print(" Hello World!"); 
      }
   }
+class Demo B
+  {
+     public static void main(...)
+     {
+         s.o.print(" Hello World!");
+         s.o.print(" Hello World!");
+     }
+  }
+
