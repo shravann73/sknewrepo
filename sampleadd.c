d09170858e2abc6261c90ca3bbf838163afb7343
root <root@ip-172-31-46-54.us-east-2.compute.internal>
2023-07-26 13:05:38 +0000

    sampleadd.c

--- sampleadd.c
+++ sampleadd.c
@@ -9,3 +9,13 @@ int mainaddoperation()
     
     return 0;
 }
+int mainmultiplyoperation()
+{
+    int num1 = 10, num2 = 20, res;
+
+    res = num1 * num2;
+    printf("multiple of numbers %d + %d = %d", num1,num2,res);
+
+    return 0;
+}
+
